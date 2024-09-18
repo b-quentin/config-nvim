7b5dfdf38cb6ed65c818bbbe04cd68d6815f870e
bquentin <bquentin@protonmail.com>
2024-09-18 08:29:51 +0200

    patch tailwind

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -68,6 +68,20 @@ lspconfig.tsserver.setup {
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
+  filetypes = {
+    "css",
+    "eruby",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "typescriptreact",
+    "vue",
+  },
 }
 
 lspconfig.eslint.setup {
